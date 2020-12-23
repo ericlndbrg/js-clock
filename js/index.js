eea6d63d521b1ab9acd517ed9d21751c2cdefe8e
Eric <ericrlindberg@gmail.com>
2020-12-22 23:35:37 -0500

    rotated the plane, added a function to draw the arcs

--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
 function draw() {
-  // declare functions at top, is this good practice?
+  const canvas = this.document.getElementById('canvas-element');
+  const button = this.document.getElementById('clear-interval');
+  const ctx = canvas.getContext('2d');
+  let seconds = 0;
+  const pi = Math.PI;
+  const startAngle = 0;
+  let endAngle = 0;
+  const increment = pi / 30;
+  const stopAngle = 2 * pi;
+  const intervalId = this.setInterval(tick, 1000);
+  // rotating a shape around its center: (https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/rotate)
+  ctx.translate(250, 250); // move matrix's origin to shape's center
+  ctx.rotate((3 * pi) / 2); // rotate matrix clockwise by desired amount
+  ctx.translate(-250, -250); // move matrix's origin back to its starting point
   function tick() {
     this.console.log(seconds);
     // clear the canvas so that the subsequent calls to tick()
     // don't redraw the same lines over and over again
+    // clearRect(x, y, width, height)
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(250, 250, 150, startAngle, endAngle, false);
-    ctx.stroke();
+    drawArc(150);
+    drawArc(100);
+    drawArc(50);
+    function drawArc(radius) {
+      ctx.beginPath();
+      // arc(x, y, radius, startAngle, endAngle, anticlockwise)
+      ctx.arc(250, 250, radius, startAngle, endAngle, false);
+      ctx.stroke();
+      return undefined;
+    }
     endAngle += increment;
     seconds += 1;
     if (endAngle > stopAngle) {
@@ -22,20 +43,9 @@ function draw() {
     this.console.log('clearInterval fired');
     return undefined;
   }
-  const canvas = this.document.getElementById('canvas-element');
-  const button = this.document.getElementById('clear-interval');
-  const ctx = canvas.getContext('2d');
-  // arc(x, y, radius, startAngle, endAngle, anticlockwise)
-  // clearRect(x, y, width, height)
-  let seconds = 0;
-  const pi = Math.PI;
-  const startAngle = 0;
-  let endAngle = 0;
-  const increment = pi / 30;
-  const stopAngle = 2 * pi;
-  const intervalId = this.setInterval(tick, 1000);
   button.addEventListener('click', function() {
     clearTickInterval();
+    return undefined;
   });
   return undefined;
 }
